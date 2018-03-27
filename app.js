49e75e9206941d39bc07ea177abf442ca40dd586
AmberKrause <amberkrause75@gmail.com>
2018-03-26 19:15:18 -0500

    basic markers and clustering based on json data

--- app.js
+++ app.js
@@ -3,21 +3,32 @@
 (function() {
   var app = angular.module("app", []);
 
+  // JSON object with Air Quality Data;
+  // initialized in the TableController function.
+  var aqData;
+
   app.controller("MapController", function($scope) {
     var map; //Map object
     var input; //input text box
     var searchBox; //SearchBox object
+    var markerCluster; //controls markers and clustering
 
-    console.log("In the Map controller.");
+    //MAP INITIALIZATION START
 
-    //initialize the map
-    $scope.latlng = new google.maps.LatLng(45, -93);
+    $scope.latlng = new google.maps.LatLng(45, -100);
     map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
       center: $scope.latlng
     });
+
+    //MAP INITIALIZATION END
+
+
+
+
+    //SEARCHBOX START
+
     input = document.getElementById("loc-input");
-    //input.value = "(45, -93)";
 
     //add map control SearchBox
     searchBox = new google.maps.places.SearchBox(input);
@@ -28,7 +39,7 @@
       searchBox.setBounds(map.getBounds());
     });
 
-    //listen for user to search
+    //listen for user to search with SearchBox
     searchBox.addListener("places_changed", function() {
       var places = searchBox.getPlaces();
 
@@ -49,7 +60,12 @@
         }
       });
       map.fitBounds(bounds);
-    });
+    });//places_changed listener
+
+    //SEARCHBOX END
+
+
+
 
 /*
     map.addListener("drag", ()=> {
@@ -59,15 +75,71 @@
       //how to do with data binding???
     });
 */
+
+
+    //MARKERS START
+
+    function updateMarkers() {
+      //create some labels
+      var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+      //clear current markers
+      if(markerCluster != undefined) {
+        markerCluster.clearMarkers();
+      }
+
+      //create array of markers
+      var markers = aqData.map(function(measure, i) {
+        var lat = aqData[i].coordinates.latitude;
+        console.log("TESTING: Latitude for marker:" + lat);
+        var lon = aqData[i].coordinates.longitude;
+        return new google.maps.Marker({
+          position: new google.maps.LatLng(lat,lon),
+          label: labels[i % labels.length]
+        });
+      });
+
+      //add marker clusterer to manage the markers
+      markerCluster = new MarkerClusterer(map, markers, {imagePath: "images/m"});
+
+    } //updateMarkers
+
+    $scope.$on("data-ready", function(event) {
+      //console.log("TESTING: inside receiver");
+      updateMarkers();
+    });
+
+    //MARKERS END
+
 }); // MapController
 
 // https://api.openaq.org/v1/measurements?coordinates=18.65,76.90&radius=500000
 app.controller("TableController", function($scope, $http) {
-    $http.get("https://api.openaq.org/v1/measurements?coordinates=18.65,76.90&radius=500000")
-    .then(function (response) { $scope.measurements = response.data.results; });
+
+    // Real URL:
+    // https://api.openaq.org/v1/measurements?coordinates=18.65,76.90&radius=50000
+
+    // But for now, that data is at
+    // https://emilymeuer.github.io/AirQualityWebApp/measurements.json
+    // to avoid this error:
+    // "The 'Access-Control-Allow-Origin' header has a value 'null' that is not
+    // equal to the supplied origin. Origin 'null' is therefore not allowed access."
+    $http.get("https://emilymeuer.github.io/AirQualityWebApp/measurements.json?coordinates=18.65,76.90&radius=500000")
+    .then(function (response) {
+        aqData              = response.data.results;
+        $scope.measurements = aqData;
+
+        //notify MapController to update markers
+        $scope.$parent.$broadcast("data-ready");
+    });
 
     //console.log("$scope.measurements = " + $scope.measurements)
 }); // TableController
 
+/*
+function mapMoved() {
+    console.log("Map moved!");
+}
+*/
 
 })();
